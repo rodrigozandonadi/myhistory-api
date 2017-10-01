755d1fe59a4966a42891d61c5467b9754575e80d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2017-09-30 23:44:25 -0300

    Mudanças no arquivo app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var multiparty = require('connect-multiparty');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +21,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  
+    next();
+  });
 
 app.use('/', index);
 app.use('/users', users);
